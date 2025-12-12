26fb3d29f88d9f091152cb88289583b2869f4aa3
Apoorva Kharga <apoorvakharga@gmail.com>
2025-12-12 13:42:16 +0530

    Initial commit

---
+++ src/GradeChecker.java
@@ -0,0 +1,19 @@
+public class GradeChecker {
+        public static void main(String[] args) {
+            char grade = 'A';
+
+            if (grade == 'O') {
+                System.out.println("Grade: O");
+            } else if (grade == 'E') {
+                System.out.println("Grade: E");
+            } else if (grade == 'A') {
+                System.out.println("Grade: A");
+            } else if (grade == 'B') {
+                System.out.println("Grade: B");
+            } else if (grade == 'C') {
+                System.out.println("Grade: C");
+            } else {
+                System.out.println("Invalid Grade");
+            }
+        }
+}

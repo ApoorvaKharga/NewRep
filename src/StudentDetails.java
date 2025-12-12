384453d30ec2a8bfac3dde890b2e536c1840daac
Apoorva Kharga <apoorvakharga@gmail.com>
2025-12-12 13:23:09 +0530

    Initial commit

--- src/StudentDetails.java
+++ src/StudentDetails.java
@@ -1,2 +1,14 @@
 public class StudentDetails {
+    public static void main(String[] args) {
+
+        String name = "Apoorva Kharga";
+        String rollNo = "24155233";
+        String section = "CSE-46";
+        String branch = "CSE(AI-ML)";
+
+        System.out.println("Display Name: " + name);
+        System.out.println("Display Roll No: " + rollNo);
+        System.out.println("Display Section: " + section);
+        System.out.println("Display Branch: " + branch);
+    }
 }
